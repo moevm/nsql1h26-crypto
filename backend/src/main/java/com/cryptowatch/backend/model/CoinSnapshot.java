c0a01b9872ce43b4afead6f20b627c7a2dfa1639
Sepesch <sp1.8@mail.ru>
2026-04-29 16:36:48 +0300

    snapshot and statistics indexation. Issue 47

--- backend/src/main/java/com/cryptowatch/backend/model/CoinSnapshot.java
+++ backend/src/main/java/com/cryptowatch/backend/model/CoinSnapshot.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
@@ -14,6 +15,7 @@ import java.util.Date;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@CompoundIndex(name = "symbol_timestamp_idx", def = "{'symbol': 1, 'timestamp': -1}")
 public class CoinSnapshot {
     @Id
     private String id;
